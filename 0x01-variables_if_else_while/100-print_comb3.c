62ebfd52e92399dfc4c668b8b04683c8c3153764
Shyaka <shyakanrleonce@gmail.comlcksnjl/k;mlf,'G:bc/V>
2023-02-19 07:19:27 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
- * main - main block
- * Description: write a program that prints all possible 
- * different combinations of two digits
- * Numbers must be separated by ,, followed by space
- * Return: 0
+ * main - Entry point
+ * Description: prints two digits combination
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int c;
-	int d = 0;
+	int c, i;
 
-	while (d < 10)
+	for (c = '0'; c <= '9'; c++)
 	{
-		c = 0;
-		while (c < 10)
+		for (i = '0'; i <= '9'; i++)
 		{
-			if (d != c && d < c)
+			if (c < i)
 			{
-				putchar('0' + d);
-				putchar('0' + c);
-				if (c + d != 17)
+				putchar(c);
+				putchar(i);
+
+				if (c != '8' || (c == '8' && i != '9'))
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-			c++
 		}
-		d++
 	}
 	putchar('\n');
 	return (0);
