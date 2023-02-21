a10ef0a8e698670aa770e8ecdbd1b64b7a401ea3
Shyaka <shyakanrleonce@gmail.comlcksnjl/k;mlf,'G:bc/V>
2023-02-21 07:09:05 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints signs
+ * @c: function parameter
+ * Return: 1, 0 and -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else 
+	{
+		_putchar(48);
+		return (0);
+	}
+}
