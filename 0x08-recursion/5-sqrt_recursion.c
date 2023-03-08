b9557ac8ad1d7f4dbf58047a62f0fea5bb724ea2
Shyaka <shyakanrleonce@gmail.comlcksnjl/k;mlf,'G:bc/V>
2023-03-08 07:47:46 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,44 @@
 #include "main.h"
-
 /**
- * _pow_recursion - function declaration
- * @x: integer to power
- * @y: power integer
- * Return: integer value
+ *_evaluate - evaluate function sqrt
+ *@i: interger
+ *@n: interger
+ *Return: evaluate sqrt
  */
 
-int _pow_recursion(int x, int y);
-
-/**
- * _pow_recursion - function definition
- * @x: the integer to power
- * @y: the power to raise to
- * Description: raise x to power y
- * Return: an integer value
- */
-
-int _pow_recursion(int x, int y)
+int _evaluate(int i, int n)
 {
-	if (y < 0)
-	{
-		return (-1);
-	}
-	else if (y > 0)
-	{
-		return (x * _pow_recursion(x, y - 1));
-	}
-	else
-	{
-		return (1);
-	}
+/*Evaluate function*/
+if (n == 0 || n == 1)
+return (n);
+
+else if (i * i < n)
+return (_evaluate(i + 1, n));
+
+else if (i * i == n) /*Condiction base*/
+return (i);
+
+return (-1);
+
+return (-1);
+}
+
+/**
+ *_sqrt_recursion - evaluate sqrt
+ *@n: interger
+ *Return: Sgrt_recursion
+ */
+
+int _sqrt_recursion(int n)
+{
+int i = 0;
+
+if (i < 0) /*If n is negative*/
+return (-1);
+
+else
+{
+return (_evaluate(i, n)); /*Recursive call*/
+}
+
 }
