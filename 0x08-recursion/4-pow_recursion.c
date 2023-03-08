6c25235b0359f0a0b8d9bd148215b120bd9a35a9
Shyaka <shyakanrleonce@gmail.comlcksnjl/k;mlf,'G:bc/V>
2023-03-08 06:23:30 -0800

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion - power of nubmer
+ * @x: the base
+ * @y: the power
+ * Return: power of nubmer
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 1)
+{
+return (x);
+}
+else if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, (y - 1)));
+}
